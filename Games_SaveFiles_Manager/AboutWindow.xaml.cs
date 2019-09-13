fb2c48b9e9f130d62abce545b9b65ffa1bf2960a
CrazyProgrammer <marcin.surma@protonmail.com>
2019-09-13 11:11:43 +0200

    Dodaj pliki projektów.

---
+++ Games_SaveFiles_Manager/AboutWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Deployment.Application;
+
+namespace Games_SaveFiles_Manager
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AboutWindow.xaml
+    /// </summary>
+    public partial class AboutWindow : Window
+    {
+        public AboutWindow()
+        {
+            InitializeComponent();
+            Get_App_Version();
+        }
+
+        public void Get_App_Version()
+        {
+            string version = null;
+
+            try
+            {
+                version = ApplicationDeployment.CurrentDeployment.CurrentVersion.ToString();
+            }
+            catch (InvalidDeploymentException)
+            {
+                MessageBox.Show("Failed to read application's version!", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                version = "unknown";
+            }
+            finally
+            {
+                version_label.Content = version;
+            }
+        }
+    }
+}
---
+++ Games_SaveFiles_Manager/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Games_SaveFiles_Manager
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Menu_about_button_Click(object sender, RoutedEventArgs e)
+        {
+            AboutWindow about_window = new AboutWindow();
+            about_window.ShowDialog();
+        }
+
+        private void Menu_quit_button_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Apply_profile_button_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Apply_profile_button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
